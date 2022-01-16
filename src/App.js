9124696f278e4e36017d4bcccecf3810b19b9783
GarudaProjects <80512930+GarudaProjects@users.noreply.github.com>
2022-01-16 23:33:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -211,7 +211,7 @@ function App() {
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
-            <StyledImg alt={"example"} src={"/config/images/example.gif"} />
+            <StyledImg alt={"example"} src={"/config/images/Astrozens-1.gif"} />
           </s.Container>
           <s.SpacerLarge />
           <s.Container
@@ -385,7 +385,7 @@ function App() {
           <s.Container flex={1} jc={"center"} ai={"center"}>
             <StyledImg
               alt={"example"}
-              src={"/config/images/example.gif"}
+              src={"/config/images/Astrozens-1.gif"}
               style={{ transform: "scaleX(-1)" }}
             />
           </s.Container>
