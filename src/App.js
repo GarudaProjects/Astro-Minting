1ea8760acb09baf7369fe3c187c4ce7dc1698eb3
GarudaProjects <80512930+GarudaProjects@users.noreply.github.com>
2022-01-16 21:35:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -222,7 +222,6 @@ function App() {
               backgroundColor: "var(--accent)",
               padding: 24,
               borderRadius: 24,
-              border: "2px dashed var(--secondary)",
               boxShadow: "0px 5px 11px 2px rgba(0,0,0,0.7)",
             }}
           >
